5d706fb81ab695b91190b0e89fa0ed425dd04a96
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2024-11-11 20:31:07 -0500

    picture update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <h1 className="text-4xl font-bold text-gray-800 mb-6">About Me</h1>
       <div className="flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-6">
         <img
-          src="public/nico-photo.jpg" // Replace with the actual URL of your photo
+          src="/nico-photo.jpg" // Replace with the actual URL of your photo
           alt="Your Name"
           className="w-40 h-40 rounded-full object-cover shadow-lg"
         />
